10b3bd9ee84d1916d1d562f2eb898cd32fee2a2d
dieterichelizabeth <dieterichelizabeth@gmail.com>
2022-04-01 01:54:00 -0500

    moves button handling to index.html, updates home-hero styling

--- public/mockups/home-page/script.js
+++ public/mockups/home-page/script.js
@@ -1,15 +1,3 @@
-// VARIABLES FOR THE PREVIOUS BUTTONS
-var prevLiveEl = document.getElementById("prevLive");
-var prevAquaticEl = document.getElementById("prevAquatic");
-var prevAfricaEl = document.getElementById("prevAfrica");
-var prevArctic4El = document.getElementById("prevArctic");
-
-// VARIABLES FOR THE NEXT BUTTONS
-var nextLiveEl = document.getElementById("nextLive");
-var nextAquaticEl = document.getElementById("nextAquatic");
-var nextAfricaEl = document.getElementById("nextAfrica");
-var nextArcticEl = document.getElementById("nextArctic");
-
 // VARIABLE/CALLS TO FORMAT SLIDES ON PAGE LOAD
 let camGroupIndex = 1;
 showLiveCams(camGroupIndex);
@@ -62,8 +50,6 @@ function moveGroups(groupNumber, parentGroupName) {
 
 // Sliders functionality (applied by parent.id of buttons)
 function scrollGroups(groupNumber, parentGroupName) {
-  console.log("Scrolling: " + parentGroupName + "-cam-groups");
-
   let cams = document.getElementsByClassName(parentGroupName + "-cam-groups");
   // If next is clicked on the last Cam Group in the index, the slider will display Cam Group 1
   if (groupNumber > cams.length) {
@@ -81,51 +67,3 @@ function scrollGroups(groupNumber, parentGroupName) {
   // displays the current cam group
   cams[camGroupIndex - 1].style.display = "block";
 }
-
-// EVENT LISTENERS FOR THE PREVIOUS BUTTONS - HOMEPAGE SCROLL
-prevLiveEl.addEventListener("click", function () {
-  moveGroups(-1, "live");
-});
-prevAquaticEl.addEventListener("click", function () {
-  moveGroups(-1, "aquatic");
-});
-prevAfricaEl.addEventListener("click", function () {
-  moveGroups(-1, "africa");
-});
-prevArctic4El.addEventListener("click", function () {
-  moveGroups(-1, "arctic");
-});
-
-// EVENT LISTENERS FOR THE NEXT BUTTONS - HOMEPAGE SCROLL
-nextLiveEl.addEventListener("click", function () {
-  moveGroups(1, "live");
-});
-nextAquaticEl.addEventListener("click", function () {
-  moveGroups(1, "aquatic");
-});
-nextAfricaEl.addEventListener("click", function () {
-  moveGroups(1, "africa");
-});
-nextArcticEl.addEventListener("click", function () {
-  moveGroups(1, "arctic");
-});
-
-/*
-COMPLETED:
-  - CREATE THE STYLING FOR EACH ELEMENT OF THE SLIDERS
-  - REVIEW NAMES OF VARIABLES/FUNCTIONS
-  - CREATE BUTTONS WITH FONT AWESOME ICONS
-  - CREATE EVENT HANDLERS TO HANDLE BUTTON CLICKS
-  - UPDATE FUNCTION TO HANDLE BUTTON CLICKS/COMMENTS DESCRIBING FUNCTIONALITY
-  - STYLE IMAGES
-  - UPDATE THE BUTTON HANDLERS TO GET THE ID OF THE SLIDERS CONTAINER
-  - CREATE A WAY FOR JAVASCRIPT STYLE.DISPLAYS TO TARGET EACH CONTAINER SEPERATELY
-  - CREATE A WAY FOR BUTTONS TO INTERACT WITH THEIR OWN CONTAINERS
-    -BOTH OF THESE FUNCTIONALITIES HAPPEN IN SHOW GROUPS FUNCTION 
-  - REFACTOR THE CODE SO THE SCRIPT FILE PULLS BY ID AND STYLES BY CLASS
-    -UNFORTUNATLY, CLASS NAME NEEDS TO REMAIN FOR THE MULTIPLEe DIVS
-  - CONDENSE THE LOGIC FOR SCROLL GROUPS FUNCTION
-  - REVIEW ELEMENT NAMES
-  - CREATE SLIDERS FOR 4 GROUPS TOTAL
-
- */
