1b6813898b5e7c3b16845fc64bd215d6dc83ced4
dieterichelizabeth <dieterichelizabeth@gmail.com>
2022-04-01 00:54:51 -0500

    refactors script.js and class names

--- public/mockups/home-page/script.js
+++ public/mockups/home-page/script.js
@@ -1,128 +1,114 @@
-var previousEl = document.getElementById("previous");
-var previous2El = document.getElementById("previous2");
-var previous3El = document.getElementById("previous3");
-var previous4El = document.getElementById("previous4");
+// VARIABLES FOR THE PREVIOUS BUTTONS
+var prevLiveEl = document.getElementById("prevLive");
+var prevAquaticEl = document.getElementById("prevAquatic");
+var prevAfricaEl = document.getElementById("prevAfrica");
+var prevArctic4El = document.getElementById("prevArctic");
 
-var nextEl = document.getElementById("next");
-var next2El = document.getElementById("next2");
-var next3El = document.getElementById("next3");
-var next4El = document.getElementById("next4");
+// VARIABLES FOR THE NEXT BUTTONS
+var nextLiveEl = document.getElementById("nextLive");
+var nextAquaticEl = document.getElementById("nextAquatic");
+var nextAfricaEl = document.getElementById("nextAfrica");
+var nextArcticEl = document.getElementById("nextArctic");
 
-// console.log(element, element2);
-function allGroups() {
-  let camGroupIndex = 1;
-  // Calls show groups to initially format the scrolls section on page load
-  showLiveCams(camGroupIndex);
-  showAquaticCams(camGroupIndex);
-  showAfricaCams(camGroupIndex);
-  showArcticCams(camGroupIndex);
+// VARIABLE/CALLS TO FORMAT SLIDES ON PAGE LOAD
+let camGroupIndex = 1;
+showLiveCams(camGroupIndex);
+showAquaticCams(camGroupIndex);
+showAfricaCams(camGroupIndex);
+showArcticCams(camGroupIndex);
 
-  // Next/previous controls
-  function moveGroups(groupNumber, parentID) {
-    scrollGroups((camGroupIndex += groupNumber), parentID);
+// FORMAL LIVE SLIDES ON PAGE LOAD
+function showLiveCams(camGroupIndex) {
+  let cams = document.getElementsByClassName("live-cam-groups");
+  // hides the slides that are not being displayed
+  for (i = 0; i < cams.length; i++) {
+    cams[i].style.display = "none";
   }
-
-  // Display the live cams on page load
-  function showLiveCams(camGroupIndex) {
-    let cams = document.getElementsByClassName("live-cam-groups");
-    // hides the cam groups that are not being displayed
-    for (i = 0; i < cams.length; i++) {
-      cams[i].style.display = "none";
-    }
-    // displays the current cam group
-    cams[camGroupIndex - 1].style.display = "block";
-  }
-
-  // Display the aquatic cams on page load
-  function showAquaticCams(camGroupIndex) {
-    let cams = document.getElementsByClassName("aquatic-cam-groups");
-    // hides the cam groups that are not being displayed
-    for (i = 0; i < cams.length; i++) {
-      cams[i].style.display = "none";
-    }
-    // displays the current cam group
-    cams[camGroupIndex - 1].style.display = "block";
-  }
-
-  // Display the aquatic cams on page load
-  function showAfricaCams(camGroupIndex) {
-    let cams = document.getElementsByClassName("africa-cam-groups");
-    // hides the cam groups that are not being displayed
-    for (i = 0; i < cams.length; i++) {
-      cams[i].style.display = "none";
-    }
-    // displays the current cam group
-    cams[camGroupIndex - 1].style.display = "block";
-  }
-
-  // Display the aquatic cams on page load
-  function showArcticCams(camGroupIndex) {
-    let cams = document.getElementsByClassName("arctic-cam-groups");
-    // hides the cam groups that are not being displayed
-    for (i = 0; i < cams.length; i++) {
-      cams[i].style.display = "none";
-    }
-    // displays the current cam group
-    cams[camGroupIndex - 1].style.display = "block";
-  }
-
-  // Sliders functionality (applied by parent.id of buttons)
-  function scrollGroups(groupNumber, parentID) {
-    console.log("Scrolling: " + parentID + "-cam-groups");
-
-    let cams = document.getElementsByClassName(parentID + "-cam-groups");
-    // If next is clicked on the last Cam Group in the index, the slider will display Cam Group 1
-    if (groupNumber > cams.length) {
-      camGroupIndex = 1;
-    }
-    // If previous is clicked on Cam Group 1, the slider will display the last Cam Group in the index
-    if (groupNumber < 1) {
-      camGroupIndex = cams.length;
-    }
-    // hides the cam groups that are not being displayed
-    for (i = 0; i < cams.length; i++) {
-      cams[i].style.display = "none";
-    }
-
-    // displays the current cam group
-    cams[camGroupIndex - 1].style.display = "block";
-  }
-
-  // event listener for Previous button
-  previousEl.addEventListener("click", function buttonHandler(event) {
-    moveGroups(-1, event.target.parentElement.parentElement.id);
-  });
-  // event listener for Next button
-  nextEl.addEventListener("click", function buttonHandler(event) {
-    moveGroups(1, event.target.parentElement.parentElement.id);
-  });
-  // event listener for Previous button
-  previous2El.addEventListener("click", function buttonHandler(event) {
-    moveGroups(-1, event.target.parentElement.parentElement.id);
-  });
-  // event listener for Next button
-  next2El.addEventListener("click", function buttonHandler(event) {
-    moveGroups(1, event.target.parentElement.parentElement.id);
-  });
-  // event listener for Previous button
-  previous3El.addEventListener("click", function buttonHandler(event) {
-    moveGroups(-1, event.target.parentElement.parentElement.id);
-  });
-  // event listener for Next button
-  next3El.addEventListener("click", function buttonHandler(event) {
-    moveGroups(1, event.target.parentElement.parentElement.id);
-  });
-  // event listener for Previous button
-  previous4El.addEventListener("click", function buttonHandler(event) {
-    moveGroups(-1, event.target.parentElement.parentElement.id);
-  });
-  // event listener for Next button
-  next4El.addEventListener("click", function buttonHandler(event) {
-    moveGroups(1, event.target.parentElement.parentElement.id);
-  });
+  // displays the current slide
+  cams[camGroupIndex - 1].style.display = "block";
 }
 
-allGroups();
+// FORMAT AQUATIC SLIDES ON PAGE LOAD
+function showAquaticCams(camGroupIndex) {
+  let cams = document.getElementsByClassName("aquatic-cam-groups");
+  for (i = 0; i < cams.length; i++) {
+    cams[i].style.display = "none";
+  }
+  cams[camGroupIndex - 1].style.display = "block";
+}
+
+// FORMAT AFRICA SLIDES ON PAGE LOAD
+function showAfricaCams(camGroupIndex) {
+  let cams = document.getElementsByClassName("africa-cam-groups");
+  for (i = 0; i < cams.length; i++) {
+    cams[i].style.display = "none";
+  }
+  cams[camGroupIndex - 1].style.display = "block";
+}
+
+// FORMAT ARCTIC SLIDES ON PAGE LOAD
+function showArcticCams(camGroupIndex) {
+  let cams = document.getElementsByClassName("arctic-cam-groups");
+  for (i = 0; i < cams.length; i++) {
+    cams[i].style.display = "none";
+  }
+  cams[camGroupIndex - 1].style.display = "block";
+}
+
+// NEXT/PREVOUS LOGIC- PASSED TO SCROLL FUNCTIONS
+function moveGroups(groupNumber, parentGroupName) {
+  scrollGroups((camGroupIndex += groupNumber), parentGroupName);
+}
+
+// Sliders functionality (applied by parent.id of buttons)
+function scrollGroups(groupNumber, parentGroupName) {
+  console.log("Scrolling: " + parentGroupName + "-cam-groups");
+
+  let cams = document.getElementsByClassName(parentGroupName + "-cam-groups");
+  // If next is clicked on the last Cam Group in the index, the slider will display Cam Group 1
+  if (groupNumber > cams.length) {
+    camGroupIndex = 1;
+  }
+  // If previous is clicked on Cam Group 1, the slider will display the last Cam Group in the index
+  if (groupNumber < 1) {
+    camGroupIndex = cams.length;
+  }
+  // hides the cam groups that are not being displayed
+  for (i = 0; i < cams.length; i++) {
+    cams[i].style.display = "none";
+  }
+
+  // displays the current cam group
+  cams[camGroupIndex - 1].style.display = "block";
+}
+
+// EVENT LISTENERS FOR THE PREVIOUS BUTTONS - HOMEPAGE SCROLL
+prevLiveEl.addEventListener("click", function () {
+  moveGroups(-1, "live");
+});
+prevAquaticEl.addEventListener("click", function () {
+  moveGroups(-1, "aquatic");
+});
+prevAfricaEl.addEventListener("click", function () {
+  moveGroups(-1, "africa");
+});
+prevArctic4El.addEventListener("click", function () {
+  moveGroups(-1, "arctic");
+});
+
+// EVENT LISTENERS FOR THE NEXT BUTTONS - HOMEPAGE SCROLL
+nextLiveEl.addEventListener("click", function () {
+  moveGroups(1, "live");
+});
+nextAquaticEl.addEventListener("click", function () {
+  moveGroups(1, "aquatic");
+});
+nextAfricaEl.addEventListener("click", function () {
+  moveGroups(1, "africa");
+});
+nextArcticEl.addEventListener("click", function () {
+  moveGroups(1, "arctic");
+});
 
 /*
 COMPLETED:
@@ -139,8 +125,7 @@ COMPLETED:
   - REFACTOR THE CODE SO THE SCRIPT FILE PULLS BY ID AND STYLES BY CLASS
     -UNFORTUNATLY, CLASS NAME NEEDS TO REMAIN FOR THE MULTIPLEe DIVS
   - CONDENSE THE LOGIC FOR SCROLL GROUPS FUNCTION
-
-TO DO:
   - REVIEW ELEMENT NAMES
   - CREATE SLIDERS FOR 4 GROUPS TOTAL
+
  */
