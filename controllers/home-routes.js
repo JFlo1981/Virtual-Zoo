eac7cb874f6116a202c0e9baff3cf9b1416f1a6b
dieterichelizabeth <dieterichelizabeth@gmail.com>
2022-04-02 03:59:02 -0500

    Fixes nav bar, Translates mockups to handlebars

---
+++ controllers/home-routes.js
@@ -0,0 +1,14 @@
+// const router = require("express").Router();
+
+// router.get("/", (req, res) => {
+//   // Once the database setup is merged, pass video objects to the homepage template
+//   res.render("homepage");
+// });
+
+// router.get("/dashboard", (req, res) => {
+//   res.render("dashboard");
+// });
+
+// module.exports = router;
+
+// Used from Jarod's branch for testing, will delete for merge w/ sandbox branch
---
+++ controllers/index.js
@@ -0,0 +1,13 @@
+// const router = require("express").Router();
+
+// const homeRoutes = require("./home-routes.js");
+
+// router.use("/", homeRoutes);
+
+// router.use((req, res) => {
+//   res.status(404).end();
+// });
+
+// module.exports = router;
+
+// Used from Jarod's branch for testing, will delete for merge w/ sandbox branch
