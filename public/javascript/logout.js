d4c4bb7e18e5b72024919e139ff014d67c309d52
dieterichelizabeth <dieterichelizabeth@gmail.com>
2022-04-05 12:36:23 -0500

    adds category descriptions/imagePaths

--- public/javascript/logout.js
+++ public/javascript/logout.js
@@ -1,4 +1,5 @@
 async function logout() {
+  console.log("===========  LOGGING OUT  ===========");
   const response = await fetch("/api/users/logout", {
     method: "post",
     headers: { "Content-Type": "application/json" },
--- seeds/category-seeds.js
+++ seeds/category-seeds.js
@@ -10,21 +10,39 @@ const categorydata = [
   },
   {
     title: "Canine Friends",
+    description:
+      "Everyone knows man's best friends spark joy. Join these adorable bundles of happiness!",
+    imagePath: "../images/page_Canine.png",
   },
   {
     title: "Fish & Sea",
+    description:
+      " Brighten up your day with the vibrantly-colored fish and other creatures of the sea. Deep dive into our vitural aquarium.",
+    imagePath: "../images/page_Fish.png",
   },
   {
     title: "Baby Animals",
+    description:
+      "Your favorite animals, but mini. Checkout some of the world's cutest babies!",
+    imagePath: "../images/page_Baby.png",
   },
   {
     title: "Horse & Hoofed",
+    description:
+      " Artiodactyl, any member of the mammalian order Artiodactyla, or even-toed ungulates, which includes the pigs, peccaries, hippopotamuses, camels, chevrotains, deer, giraffes, pronghorn, antelopes, sheep, goats, and cattle.",
+    imagePath: "../images/page_Hoofed.png",
   },
   {
-    title: "Featheref Friends",
+    title: "Feathered Friends",
+    description:
+      "The fossil record shows that birds evolved alongside the dinosaurs during the Jurassic period 160 million years ago. The best known fossil is archaeopteryx, which was about the size of a crow.",
+    imagePath: "../images/page_Birds.png",
   },
   {
     title: "Creepy Favorites",
+    description:
+      "All the things that make your skin crawl. Check out these videos, if you're into that.",
+    imagePath: "../images/page_Creepy.png",
   },
 ];
 
