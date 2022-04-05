24ab08d05c2492395ecf49c3d9a5bda6d3730c0d
Jarod Florence <jarod.florence@outlook.com>
2022-04-05 12:25:47 -0500

    port3006

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const exphbs = require('express-handlebars');
 const helpers = require('./utils/helpers');
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3006;
 
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
